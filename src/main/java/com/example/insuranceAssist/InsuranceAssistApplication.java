4c5d13b6dfb0e9cb7cc5ded0908fd851c87b4014
Arghya-Banerjee-CTS <Arghya.Banerjee@cognizant.com>
2025-08-30 13:07:03 +0530

    Initial Commit

---
+++ src/main/java/com/example/insuranceAssist/InsuranceAssistApplication.java
@@ -0,0 +1,13 @@
+package com.example.insuranceAssist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/insuranceAssist/InsuranceAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insuranceAssist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
