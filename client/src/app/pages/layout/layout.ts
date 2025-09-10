ceaa6c7afba876d59bbfab8688d188d8e58e0c28
Philip Bulla <2438073@cognizant.com>
2025-09-10 10:48:43 +0530

    Dashboard parent layout added

---
+++ client/src/app/pages/layout/layout.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css'
+})
+export class Layout {
+  
+  router = inject(Router);
+  OnLogoff(){
+    this.router.navigate(['/login']);
+  }
+}
